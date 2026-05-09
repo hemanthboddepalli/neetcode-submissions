90fcfa61a52b74e9a5c3497c86bb4e94953c0001
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-05-09 18:21:21 +0530

    Add: confusing-number - submission-2

---
+++ Data Structures & Algorithms/confusing-number/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean confusingNumber(int n) {
+        int[] digits = new int[10];
+        Arrays.fill(digits, -1);
+        digits[0] = 0; digits[1] = 1; digits[6] = 9; digits[8] = 8; digits[9] = 6;
+        int copyN = n;
+        long rotatedN = 0;
+
+        while (copyN > 0) {
+            int res = copyN % 10;
+            if (digits[res] == -1) {
+                return false;
+            }
+            rotatedN = rotatedN * 10 + digits[res];
+            copyN = copyN / 10;
+        }
+
+        return n != rotatedN;
+    }
+}
