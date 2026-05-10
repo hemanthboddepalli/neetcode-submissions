1ab637ddd5a07ef56cf57b07be6fd76a034aa823
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-05-10 20:08:38 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int element = -1;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                element = nums[i];
+                count++;
+            } else if (element == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return element;
+    }
+}
