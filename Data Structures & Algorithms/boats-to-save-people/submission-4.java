bbe5c47ee2aea53f6dcb1b41c7975b9ecdf9e15a
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-05-10 14:19:18 +0530

    Add: boats-to-save-people - submission-4

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int boats = 0, left = 0, right = people.length - 1;
+        while (left <= right) {
+            right--;
+            boats++;
+            if (people[left] + people[right + 1] <= limit) {
+                left++;
+            }
+        }
+
+        return boats;
+    }
+}
