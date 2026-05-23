66e98b94a0e8921312652c1f811ffeea26ab796d
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-05-23 10:46:12 +0530

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.java
@@ -0,0 +1,44 @@
+class MyHashSet {
+    private LinkedList<Integer>[] set;
+    private static final int SIZE = 1000;
+
+    public MyHashSet() {
+        set = new LinkedList[SIZE];
+    }
+    
+    public void add(int key) {
+        int index = key % SIZE;
+
+        if (set[index] == null) {
+            set[index] = new LinkedList<>();
+        }
+
+        if (!set[index].contains(key)) {
+            set[index].add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        int index = key % SIZE;
+
+        if (set[index] != null) {
+            set[index].remove(Integer.valueOf(key));
+        }
+    }
+    
+    public boolean contains(int key) {
+        int index = key % SIZE;
+        if (set[index] != null && set[index].contains(key)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
