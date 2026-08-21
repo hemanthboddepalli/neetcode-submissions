2c47fa56b87641e95fbf976342b1864079f29170
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-08-22 05:20:55 +0530

    Add: remove-duplicates-from-sorted-array - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+        int right = 1;
+
+        while (right < nums.length) {
+            if (nums[right] != nums[right - 1]) {
+                left++;
+                nums[left] = nums[right];
+            }
+            right++;
+        }
+
+        return left + 1;
+    }
+}
