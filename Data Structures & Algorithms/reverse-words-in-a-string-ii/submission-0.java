72cff8a5e7cac30894251be9603f985dac90e94d
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-09-05 23:12:04 +0530

    Add: reverse-words-in-a-string-ii - submission-0

---
+++ Data Structures & Algorithms/reverse-words-in-a-string-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void reverseWords(char[] s) {
+        reverse(s, 0, s.length - 1);
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length) {
+            while (j < s.length && s[j] != ' ') j++;
+            reverse(s, i, j - 1);
+            i = j + 1;
+            j = j + 1;
+        }
+    }
+
+    public void reverse(char[] s, int left, int right) {
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
