15d6d867954a8b6a59166a03b15e8564cddd288d
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-09-05 11:59:50 +0530

    Add: sort-colors - submission-7

---
+++ Data Structures & Algorithms/sort-colors/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = 0;
+
+        while (index <= right) {
+            if (nums[index] == 0) {
+                nums[index] = nums[left];
+                nums[left] = 0;
+                left++;
+            } else if (nums[index] == 2) {
+                nums[index] = nums[right];
+                nums[right] = 2;
+                right--;
+                continue;
+            }
+            index++;
+        }
+    }
+}
