1f649782e8575b8246468a393bb9742267e5e7f2
Hemanth Boddepalli <hemanth.boddepalli@gmail.com>
2026-09-12 16:10:30 +0530

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for (int num : asteroids) {
+            if (num < 0) {
+                boolean pushRequired = true;
+                
+                while (!st.isEmpty() && st.peek() > 0 && pushRequired) {
+                    if (st.peek() < Math.abs(num)) {
+                        st.pop();
+                    } else if (st.peek() > Math.abs(num)) {
+                        pushRequired = false;
+                    } else {
+                        st.pop();
+                        pushRequired = false;
+                    }
+                }
+                if (pushRequired) {
+                    st.push(num);
+                }
+
+            } else {
+                st.push(num);
+            }
+        }
+
+        return st.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
